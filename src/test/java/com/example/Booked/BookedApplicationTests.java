eae949304cae647e2d9df47723b1e9a6ca9ffff6
pembemiriam <pembemiriam007@gmail.com>
2019-10-30 12:32:09 +0100

    initial commit

---
+++ src/test/java/com/example/Booked/BookedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Booked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
